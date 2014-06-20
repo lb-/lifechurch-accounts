5161e488ef5caee18eeaf8c4aa4320a26896dd39
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-06-20 19:32:56 +1000

    setting up reliable initial state

--- server/sever.js
+++ server/sever.js
@@ -1,18 +1,23 @@
 Meteor.startup(function() {
   console.log('started up', Date());
-  if ( Cards.find().count() === 0 ) {
+  if ( Cards.find({name: 'Anthea Robinson'}).count() === 0 ) {
     var antheaCard = Cards.insert({name: 'Anthea Robinson'});
-    console.log(antheaCardUsage);
-    var antheaCardUsage = {
-      card: antheaCard,
-      user: 'abc123',
-    };
-    //Schemas.cardUsage.clean( antheaCardUsage );
-    //console.log(antheaCardUsage);
-    var newCardUsage = CardUsages.insert(antheaCardUsage);
-    console.log(CardUsages.find({_id:newCardUsage}).fetch());
+  }
+  if ( Cards.find({name: 'Sacha Williams'}).count() === 0 ) {
     var sachaCard = Cards.insert({name: 'Sacha Williams'});
   }
+    //console.log(antheaCardUsage);
+    //var antheaCardUsage = {
+    //  card: antheaCard,
+    //  user: 'abc123',
+    //};
+    //Schemas.cardUsage.clean( antheaCardUsage );
+    //console.log(antheaCardUsage);
+    //var newCardUsage = CardUsages.insert(antheaCardUsage);
+    //console.log(CardUsages.find({_id:newCardUsage}).fetch());
+    //var sachaCard = Cards.insert({name: 'Sacha Williams'});
+
+    
   // first, remove configuration entry in case service is already configured
   ServiceConfiguration.configurations.remove({
     service: "google"
