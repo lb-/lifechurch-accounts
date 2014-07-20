a551470e73110dc146f7886451eff88abc4780d7
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-07-20 18:48:01 +1000

    Tried to move S3 config to settings
    Not fully working though

--- collections/receipts.js
+++ collections/receipts.js
@@ -1,28 +1,58 @@
-Schemas.receipt = new SimpleSchema([Schemas.baseSchema,{
-  upload: {
-    label: 'Photo',
-    type: String,
+var receiptImagesS3 = new FS.Store.S3( 'receiptsS3', {
+  region: Meteor.settings.S3.region,
+  bucket: Meteor.settings.S3.bucket,
+  accessKeyId: Meteor.settings.S3.accessKeyId,
+  secretAccessKey: Meteor.settings.S3.secretAccessKey,
+  ACL: Meteor.settings.S3.ACL,
+});
+
+Receipts = new FS.Collection( 'receipts', {
+  stores: [ receiptImagesS3 ],
+  // filter: {
+  //   allow: {
+  //     contentTypes: ['image/*', 'application/pdf']
+  //   }
+  // },
+});
+
+Receipts.allow({
+  insert: function( userId, doc ) {
+    return true;
   },
-  merchant: {
-    label: 'Merchant',
-    type: String,
-  },
-  purchaseDate: {
-    label: 'Purchase Date',
-    type: Date,
-  },
-  approvalCode: {
-    label: 'Approval Code',
-    type: String,
-  },
-  comment: {
-    label: 'Comments',
-    type: String,
-    optional: true,
-  },
-  total: {
-    label: "Receipt Total",
-    type: Number,
-    decimal: true,
-  },
-}]);
+  update: function( userId, doc ) {
+    return true;
+  }
+});
+
+// Schemas.receipt = new SimpleSchema([Schemas.baseSchema,{
+//   upload: {
+//     label: 'Photo',
+//     type: String,
+//     optional: true,
+//   },
+//   merchant: {
+//     label: 'Merchant',
+//     type: String,
+//     optional: true,
+//   },
+//   purchaseDate: {
+//     label: 'Purchase Date',
+//     type: Date,
+//     optional: true,
+//   },
+//   approvalCode: {
+//     label: 'Approval Code',
+//     type: String,
+//     optional: true,
+//   },
+//   comment: {
+//     label: 'Comments',
+//     type: String,
+//     optional: true,
+//   },
+//   total: {
+//     label: "Receipt Total",
+//     type: Number,
+//     decimal: true,
+//   },
+// }]);
